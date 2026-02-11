920978fa10f3749d306da231d8e5b576bdd404e7
eshaansdoshi-sketch <eshaansdoshi@gmail.com>
2026-02-11 22:45:51 +0530

    cpp

---
+++ Eshaan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void solve() {
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+       
+    }
+    int count = 0;
+    }
+
+int main() {
+    solve();
+    return 0;
+}
